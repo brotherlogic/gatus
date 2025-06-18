340c8908c3c17a3ebd193fc6d1f6b45fbb83ff70
brotherlogic <brotherlogic@gmail.com>
2025-06-18 14:22:51 -0700

    Better if

--- runner.sh
+++ runner.sh
@@ -4,7 +4,7 @@ cd ~/gatus
 git fetch -p
 git merge origin/main
 
-if ! $(diff .git/refs/heads/main running_version); then
+if ! diff .git/refs/heads/main running_version; then
     echo "Restarting Gataus"
     sudo docker rm gatus
     sudo docker run -d -p 8080:8080 --mount type=bind,source="$(pwd)"/config.yaml,target=/config/config.yaml --name gatus twinproduction/gatus
