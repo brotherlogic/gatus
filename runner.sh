037f588435e7434f7c0c0fe6ccf0e59e1cc073f7
brotherlogic <brotherlogic@gmail.com>
2025-06-18 14:21:59 -0700

    Only run on change, run in background

--- runner.sh
+++ runner.sh
@@ -4,10 +4,10 @@ cd ~/gatus
 git fetch -p
 git merge origin/main
 
-if $(diff .git/refs/heads/main running_version); then
+if ! $(diff .git/refs/heads/main running_version); then
     echo "Restarting Gataus"
     sudo docker rm gatus
-    sudo docker run -p 8080:8080 --mount type=bind,source="$(pwd)"/config.yaml,target=/config/config.yaml --name gatus twinproduction/gatus
+    sudo docker run -d -p 8080:8080 --mount type=bind,source="$(pwd)"/config.yaml,target=/config/config.yaml --name gatus twinproduction/gatus
 fi
 
 cp .git/refs/heads/main running_version
