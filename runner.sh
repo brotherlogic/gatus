a7c71479ea345706c6f556afbb5ea9ddc4b3dc14
brotherlogic <brotherlogic@gmail.com>
2025-06-18 14:23:27 -0700

    Stop before rm

--- runner.sh
+++ runner.sh
@@ -6,6 +6,7 @@ git merge origin/main
 
 if ! diff .git/refs/heads/main running_version; then
     echo "Restarting Gataus"
+    sudo docker stop gatus
     sudo docker rm gatus
     sudo docker run -d -p 8080:8080 --mount type=bind,source="$(pwd)"/config.yaml,target=/config/config.yaml --name gatus twinproduction/gatus
 fi
