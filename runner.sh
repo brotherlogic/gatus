fc36ef9d10d92e9b7c03206f562a37ef60f9a69d
brotherlogic <brotherlogic@gmail.com>
2025-06-18 06:46:21 -0700

    Updates runner

--- runner.sh
+++ runner.sh
@@ -6,6 +6,8 @@ git merge origin/main
 
 if $(diff .git/refs/heads/main running_version); do
     echo "Restarting Gataus"
+    sudo docker rm gatus
+    sudo docker run -p 8080:8080 --mount type=bind,source="$(pwd)"/config.yaml,target=/config/config.yaml --name gatus twinproduction/gatus
 done
 
 cp .git/refs/heads/main running_version
